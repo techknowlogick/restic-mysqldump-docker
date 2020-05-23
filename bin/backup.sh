4ad114d5816c32f70b9d042d6a16ba6a11af1784
techknowlogick <techknowlogick@gitea.io>
2020-05-23 00:39:50 -0400

    update to mysqldump

--- bin/backup.sh
+++ bin/backup.sh
@@ -6,32 +6,32 @@ setup.sh
 
 for i in {1..5}; do
 	export HOSTNAME_VAR="HOSTNAME_$i"
-	export PGHOST_VAR="PGHOST_$i"
-	export PGPASSWORD_VAR="PGPASSWORD_$i"
-	export PGPORT_VAR="PGPORT_$i"
-	export PGUSER_VAR="PGUSER_$i"
+	export DBHOST_VAR="DBHOST_$i"
+	export DBPASSWORD_VAR="DBPASSWORD_$i"
+	export DBPORT_VAR="DBPORT_$i"
+	export DBUSER_VAR="DBUSER_$i"
 
-	export HOSTNAME="${!HOSTNAME_VAR:-$PGHOST_$i}"
-	export PGHOST="${!PGHOST_VAR}"
-	export PGPASSWORD="${!PGPASSWORD_VAR}"
-	export PGPORT="${!PGPORT_VAR:-5432}"
-	export PGUSER="${!PGUSER_VAR}"
+	export HOSTNAME="${!HOSTNAME_VAR:-$DBHOST_$i}"
+	export DBHOST="${!DBHOST_VAR}"
+	export DBPASSWORD="${!DBPASSWORD_VAR}"
+	export DBPORT="${!DBPORT_VAR:-3306}"
+	export DBUSER="${!DBUSER_VAR}"
 
 	# No more databases.
-	for var in PGHOST PGUSER; do
+	for var in DBHOST DBUSER; do
 		[[ -z "${!var}" ]] && {
 			echo 'Finished backup successfully'
 			exit 0
 		}
 	done
 
-	echo "Dumping database cluster $i: $PGUSER@$PGHOST:$PGPORT"
+	echo "Dumping database cluster $i: $DBUSER@$DBHOST:$DBPORT"
 
-	# Wait for PostgreSQL to become available.
+	# Wait for MySQL to become available.
 	COUNT=0
-	until psql -l > /dev/null 2>&1; do
+	until mysqlshow -h $DBHOST -P $DBPORT -u $DBUSER -p $DBPASSWORD > /dev/null 2>&1; do
 		if [[ "$COUNT" == 0 ]]; then
-			echo "Waiting for PostgreSQL to become available..."
+			echo "Waiting for MySQL to become available..."
 		fi
 		(( COUNT += 1 ))
 		sleep 1
@@ -40,25 +40,22 @@ for i in {1..5}; do
 		echo "Waited $COUNT seconds."
 	fi
 
-	mkdir -p "/pg_dump"
+	mkdir -p "/mysqldump"
 
 	# Dump individual databases directly to restic repository.
-	DBLIST=$(psql -d postgres -q -t -c "SELECT datname FROM pg_database WHERE datname NOT IN ('postgres', 'rdsadmin', 'template0', 'template1')")
+	DBLIST=$(mysql -h $DBHOST -P $DBPORT -u $DBUSER -p $DBPASSWORD -e "SELECT schema_name from INFORMATION_SCHEMA.SCHEMATA WHERE schema_name NOT IN ('sys', 'information_schema', 'mysql', 'performance_schema')")
 	for dbname in $DBLIST; do
 		echo "Dumping database '$dbname'"
-		pg_dump --file="/pg_dump/$dbname.sql" --no-owner --no-privileges --dbname="$dbname" || true  # Ignore failures
+		mysqldump --databases $dbname --add-drop-database --triggers --routines --events --set-gtid-purged=OFF -h $DBHOST -P $DBPORT -u $DBUSER -p $DBPASSWORD  > /mysqldump/$dbname.sql || true  # Ignore failures
 	done
 
-	# echo "Dumping global objects for '$PGHOST'"
-	# pg_dumpall --file="/pg_dump/!globals.sql" --globals-only
-
 	echo "Sending database dumps to S3"
-	while ! restic backup --host "$HOSTNAME" "/pg_dump"; do
+	while ! restic backup --host "$HOSTNAME" "/mysqldump"; do
 		echo "Sleeping for 10 seconds before retry..."
 		sleep 10
 	done
 
 	echo 'Finished sending database dumps to S3'
 
-	rm -rf "/pg_dump"
+	rm -rf "/mysqldump"
 done
